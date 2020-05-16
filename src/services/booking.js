297a116d6270767daf734920783705ee04402ac5
nguyenthuy1406 <nguyenthuy14061998@gmail.com>
2020-05-17 01:16:20 +0700

    add edit state and delete booking faile

--- src/services/booking.js
+++ src/services/booking.js
@@ -11,7 +11,9 @@ import {
 //const qs = require('qs');
 export const booking = {
     bookingRoom,
-    getAllBookingHotel
+    getAllBookingHotel,
+    updateState,
+    deleteBooking
 }
 
 function bookingRoom(userId,roomId,body) {
@@ -25,6 +27,21 @@ function bookingRoom(userId,roomId,body) {
 function getAllBookingHotel(userId) {
     return responseService.fetchRetry(`${config.apiUrl}host/${userId}/booking`, {
         method: 'GET',
+        headers: authHost(),
+        mode: 'cors', 
+    }, 4)
+}
+function updateState(body, userId, bookingId) {
+    return responseService.fetchRetry(`${config.apiUrl}user/${userId}/booking/${bookingId}`, {
+        method: 'PUT',
+        headers: authHost(),
+        mode: 'cors', 
+        body: JSON.stringify(body),
+    }, 4)
+}
+function deleteBooking(userId, bookingId) {
+    return responseService.fetchRetry(`${config.apiUrl}user/${userId}/booking/${bookingId}`, {
+        method: 'DELETE',
         headers: authHost(),
         mode: 'cors', 
     }, 4)
