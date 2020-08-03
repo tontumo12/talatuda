9a6f4aad14e8260c390ea91e655d54dd3e071b34
Nguyen Ton Tu <tunt@codenet.jp>
2020-08-03 23:29:25 +0700

    tesst

--- api/db.js
+++ api/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
+  host: process.env.DB_HOST || "localhost:12345",
+  user: process.env.DB_USER || "tunt",
   password: process.env.DB_PASSWORD || "120597",
-  database: process.env.DB_NAME || "hotel"
+  database: process.env.DB_NAME || "talatuda"
 });
 
-module.exports = db
+module.exports = db;
