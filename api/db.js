68fc0509708b5c645ba23edbab5ed46e46b33390
Nguyen Ton Tu <tunt@codenet.jp>
2020-10-12 14:34:22 +0700

    fix ci/cd

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost:12345",
+  host: process.env.DB_HOST || "localhost:3306",
   user: process.env.DB_USER || "tunt",
   password: process.env.DB_PASSWORD || "120597",
   database: process.env.DB_NAME || "talatuda"
